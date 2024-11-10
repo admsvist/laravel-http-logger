588ea450ca5a950f80acd1695959d75d03bb49db
Mikhail Kalinin <new-mishut@mail.ru>
2024-11-10 20:16:36 +0500

    Update DefaultLogWriter.php

--- src/DefaultLogWriter.php
+++ src/DefaultLogWriter.php
@@ -12,18 +12,20 @@ class DefaultLogWriter implements LogWriter
 {
     protected $sanitizer;
 
-    public function logRequest(Request $request, Response $response)
+    public function logRequest(Request $request, Response $response, int $duration)
     {
-        $message = $this->formatMessage($this->getMessage($request, $response));
+        $message = $this->formatMessage($this->getMessage($request, $response, $duration));
 
         Log::channel(config('http-logger.log_channel'))->log(config('http-logger.log_level', 'info'), $message);
     }
 
-    public function getMessage(Request $request, Response $response)
+    public function getMessage(Request $request, Response $response, int $duration)
     {
         $files = (new Collection(iterator_to_array($request->files)))
             ->map([$this, 'flatFiles'])
             ->flatten();
+
+        $userId = Auth::id() ?? 'guest';
 
         return [
             'method' => strtoupper($request->getMethod()),
@@ -31,6 +33,10 @@ class DefaultLogWriter implements LogWriter
             'body' => $request->except(config('http-logger.except')),
             'headers' => $this->getSanitizer()->clean($request->headers->all(), config('http-logger.sanitize_headers')),
             'files' => $files,
+            'user_id' => $userId,
+            'response_code' => $response->getStatusCode(),
+            'error_text' => $response->exception?->getMessage(),
+            'duration_ms' => $duration,
         ];
     }
 
@@ -40,7 +46,7 @@ class DefaultLogWriter implements LogWriter
         $headersAsJson = json_encode($message['headers']);
         $files = $message['files']->implode(',');
 
-        return "{$message['method']} {$message['uri']} - Body: {$bodyAsJson} - Headers: {$headersAsJson} - Files: " . $files;
+        return "{$message['method']} {$message['uri']} - Response Code: {$message['response_code']} - Error Text: {$message['error_text']} - Duration: {$message['duration_ms']} - User: {$message['user_id']} - Headers: {$headersAsJson} - Body: {$bodyAsJson} - Files: " . $files;
     }
 
     public function flatFiles($file)
