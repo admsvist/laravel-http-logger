df6c8837302ba3adad5e63c7e9e543508fd1f99c
Mikhail Kalinin <new-mishut@mail.ru>
2024-11-10 20:31:43 +0500

    Update DefaultLogWriter.php

--- src/DefaultLogWriter.php
+++ src/DefaultLogWriter.php
@@ -3,6 +3,7 @@
 namespace Spatie\HttpLogger;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
